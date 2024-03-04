2a141df29c00aa01e895442726ce50dbcf8a252c
Eldar Abdullin <60284515+eldarovsky@users.noreply.github.com>
2024-03-04 10:10:41 +0500

    Deleted tab bar items animation.

--- The Met/Scenes/TabBarScenes/TabBar.swift
+++ The Met/Scenes/TabBarScenes/TabBar.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 // MARK: - TabBarController
-final class TabBar: UITabBarController {
+final class TabBar: UITabBarController, UITabBarControllerDelegate {
     
     // MARK: - Private properties
     private let networkManager = NetworkManager.shared
@@ -48,37 +48,5 @@ private extension TabBar {
         tabBarAppearance.stackedLayoutAppearance = tabBarItemAppearance
         tabBar.standardAppearance = tabBarAppearance
         tabBar.scrollEdgeAppearance = tabBarAppearance
-    }
-}
-
-// MARK: - UITabBarControllerDelegate
-extension TabBar: UITabBarControllerDelegate {
-    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-
-        guard let selectedTabIndex = tabBarController.viewControllers?.firstIndex(of: viewController),
-              let selectedTabBarItem = tabBar.items?[safe: selectedTabIndex] else {
-            return
-        }
-
-        if let tabItemView = selectedTabBarItem.value(forKey: "view") as? UIView,
-           let imageView = tabItemView.subviews.compactMap({ $0 as? UIImageView }).first {
-            animate(imageView: imageView)
-        }
-    }
-
-    private func animate(imageView: UIImageView) {
-        UIView.animate(withDuration: 0.1, animations: {
-            imageView.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
-        }) { _ in
-            UIView.animate(withDuration: 0.1) {
-                imageView.transform = .identity
-            }
-        }
-    }
-}
-
-extension Collection {
-    subscript(safe index: Index) -> Element? {
-        return indices.contains(index) ? self[index] : nil
     }
 }
